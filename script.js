0ccfb6f8c0a0e0fb35de45c6d7662de9ddace09f
Cody Lillywhite <codylillyw@gmail.com>
2021-02-01 21:10:25 -0500

    styled contact div, added formspree

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let codyLillywhiteh1 = document.getElementById("codyLillywhiteh1");
 let background = document.getElementById("bg");
 document.addEventListener("scroll", () => {
-    if(window.scrollY > 350) {
+    if(window.scrollY > 400) {
         codyLillywhiteh1.classList.add("titleNav");
         codyLillywhiteh1.classList.remove("center");
         background.classList.add("transitionOpacity")
     }
-    else if(window.scrollY < 300){
+    else if(window.scrollY < 350){
         codyLillywhiteh1.classList.add("center");
         codyLillywhiteh1.classList.remove("titleNav");
     }

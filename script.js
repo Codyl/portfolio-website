332b941d324469f61f146f910c4a0b398343b00b
Cody Lillywhite <codylillyw@gmail.com>
2021-02-11 21:04:14 -0500

    Bug fix: Small screen drop down

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let codyLillywhiteh1 = document.getElementById("codyLillywhiteh1");
 let background = document.getElementById("bg");
+let smallNavDisplayActive =false;
+const NAV_ANCHOR_NAMES = ["About", "Interests", "Projects", "Contact"];
+
 document.addEventListener("scroll", () => {
     if(window.scrollY > 400) {
         codyLillywhiteh1.classList.add("titleNav");
@@ -11,3 +14,29 @@ document.addEventListener("scroll", () => {
         codyLillywhiteh1.classList.remove("titleNav");
     }
 });
+document.getElementById('navIcon').addEventListener('click', () => {
+    setSmallNavDisplayActive();
+})
+function setSmallNavDisplayActive() {
+    document.getElementById('nav').style.height = "40vh";
+    for(anchor of NAV_ANCHOR_NAMES) {
+        displayNavAnchorByName(anchor);
+    }
+    document.getElementById("About").style.paddingTop = "30px";
+}
+function setSmallNavDisplayInactive() {
+    document.getElementById('nav').style.height = "6vh";
+    for(anchor of NAV_ANCHOR_NAMES) {
+    document.getElementById(anchor).remove();
+    }
+}
+function displayNavAnchorByName(anchorName) {
+    let elem = document.createElement("a");
+    elem.classList.add("anchor", "navText");
+    elem.setAttribute("id",anchorName);
+    elem.style.display = "block";
+    elem.innerText = anchorName;
+    elem.addEventListener("click", () => setSmallNavDisplayInactive());
+    elem.href = "#"+anchorName.toLowerCase();
+    document.getElementById('smallScreenNav').appendChild(elem);
+}
